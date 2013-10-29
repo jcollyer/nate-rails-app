7f02533daac5ada19f50002113960eb7ed1b47c2
jcollyer <collyerdesign@gmail.com>
2013-10-29 07:20:46 -0700

    add ajax to biblebooks new, and add teaching list on home.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Nate::Application.routes.draw do
-  get "sessions/new"
   root to: "home#index"
 
   resources :biblebooks
   resources :teachings
+  resources :users
+  resources :sessions
 
   #all for authentication ########
 
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-
-  resources :users
-  resources :sessions
 
   #################################
 end
