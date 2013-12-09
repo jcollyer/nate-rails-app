d1611e5ef047c360ed2407ee25c0106b5b06cec2
jcollyer <collyerdesign@gmail.com>
2013-12-09 06:05:28 -0800

    update home controller to add abouts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @biblebooks = Biblebook.find(:all, :order => "position ASC")
+    @abouts     = About.find(:all) #change to only show first?
   end
 
 end
