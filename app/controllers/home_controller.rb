1b57ade2b66eb1fe2f3a24ee5020639e61e56476
jcollyer <collyerdesign@gmail.com>
2013-12-18 19:58:38 -0800

    update home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
 
   def index
     @biblebooks = Biblebook.find(:all, :order => "position ASC")
-    @abouts     = About.find(:all) #change to only show first?
-    @books      = Book.find(:all)
-    @notes      = Note.find(:all) #change to only show first?
+    @abouts     = About.all #change to only show first?
+    @books      = Book.all
+    @notes      = Note.all #change to only show first?
   end
 
 end
