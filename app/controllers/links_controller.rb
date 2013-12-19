d1b0791fd98f696ccf81ff9f91e0f4e828e02e86
jcollyer <collyerdesign@gmail.com>
2013-12-18 19:58:25 -0800

    add delet to Link, and fix update - nested resource

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,8 @@
 class LinksController < ApplicationController
+  def new
+    @link = Link.new
+  end
+
   def create
     @note = Note.find(params[:note_id])
     @link = @note.links.create!(link_params)
@@ -8,6 +12,29 @@ class LinksController < ApplicationController
   def edit
     @link = Link.find(params[:id])
     @note = Note.find(params[:note_id])
+  end
+
+  def update
+    @note = Note.find(params[:note_id])
+    @link = @note.links.find(params[:id])
+    respond_to do |format|
+      if @link.update(link_params)
+        format.html { redirect_to "/notes/2/edit", notice: 'Link was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+    respond_to do |format|
+      format.html { redirect_to "/notes/2/edit" }
+      format.json { head :no_content }
+    end
   end
 
   private
