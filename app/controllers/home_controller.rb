d5db0553221ddaaacbd5824720772b54993602e0
jcollyer <collyerdesign@gmail.com>
2013-12-30 22:03:50 -0800

    add bio to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @abouts     = About.all #change to only show first?
     @books      = Book.all
     @notes      = Note.all #change to only show first?
+    @bios       = Bio.all #change to only show first?
   end
 
 end
