6ea4aaa90f6a4f8c9321a22d078bbc744d8e5a8b
jcollyer <collyerdesign@gmail.com>
2014-02-19 19:02:44 -0800

    update route file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Nate::Application.routes.draw do
 
 
 
-  resources :bios
 
   root to: "home#index"
 
@@ -10,17 +9,17 @@ Nate::Application.routes.draw do
   resources :teachings
   resources :users
   resources :sessions
+  resources :bios
   resources :abouts
   resources :books
   resources :notes do
     resources :links
   end
 
-  #all for authentication ########
-
+  ### All for authentication ###
   get "log_out"  => "sessions#destroy", :as => "log_out"
   get "log_in"   => "sessions#new",     :as => "log_in"
   get "sign_up"  => "users#new",        :as => "sign_up"
+  ##############################
 
-  #################################
 end
