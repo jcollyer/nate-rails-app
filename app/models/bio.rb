27711ea86a20ed89afa0bfb71b06f3e07f921a48
jcollyer <collyerdesign@gmail.com>
2014-02-24 09:11:02 -0800

    update medium image size on Bio

--- app/models/bio.rb
+++ app/models/bio.rb
@@ -1,7 +1,7 @@
 class Bio < ActiveRecord::Base
 
   has_attached_file :image,
-    :styles => { :large => "260x390>", :medium => "130x195>", :thumb => "65x100>" },
+    :styles => { :large => "260x390>", :medium => "200x300>", :thumb => "65x100>" },
     :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/s3.yml",
     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
