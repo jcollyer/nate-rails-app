72105e1ec8c711723690d46bc6ef8818bedced26
jcollyer <collyerdesign@gmail.com>
2014-02-27 08:38:05 -0800

    update models to use new s3 credentials

--- app/models/bio.rb
+++ app/models/bio.rb
@@ -3,7 +3,6 @@ class Bio < ActiveRecord::Base
   has_attached_file :image,
     :styles => { :large => "260x390>", :medium => "200x300>", :thumb => "65x100>" },
     :storage => :s3,
-    # :bucket => "nate_database",
     :s3_credentials => "#{Rails.root}/config/s3_cred.yml",
     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
     :url => "/system/:attachment/:id/:style/:filename",
--- app/models/teaching.rb
+++ app/models/teaching.rb
@@ -2,8 +2,9 @@ class Teaching < ActiveRecord::Base
   belongs_to :biblebook
 
   has_attached_file :mp3,
-                    :storage => :s3,
-                    :s3_credentials => "#{Rails.root}/config/s3.yml",
-                    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-                    :url => "/system/:attachment/:id/:style/:filename"
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/s3_cred.yml",
+    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+    :url => "/system/:attachment/:id/:style/:filename"
+
 end
