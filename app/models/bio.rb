272ad1462e8f98db600eff948817853f9dbbd610
jcollyer <collyerdesign@gmail.com>
2014-02-27 15:20:25 -0800

    test aws local vars stratigy

--- app/models/bio.rb
+++ app/models/bio.rb
@@ -3,7 +3,11 @@ class Bio < ActiveRecord::Base
   has_attached_file :image,
     :styles => { :large => "260x390>", :medium => "200x300>", :thumb => "65x100>" },
     :storage => :s3,
-    # :s3_credentials => "#{Rails.root}/config/s3_cred.yml",
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET'],
+      :bucket => "nate_database"
+    },
     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
     :url => "/system/:attachment/:id/:style/:filename",
     :default_url => '/assets/missing_:style.jpg'
