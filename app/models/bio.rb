495fa39b986d0a4a1829feabd3c491e8afa7b20f
jcollyer <collyerdesign@gmail.com>
2014-02-27 08:04:41 -0800

    update s3 method

--- app/models/bio.rb
+++ app/models/bio.rb
@@ -3,7 +3,7 @@ class Bio < ActiveRecord::Base
   has_attached_file :image,
     :styles => { :large => "260x390>", :medium => "200x300>", :thumb => "65x100>" },
     :storage => :s3,
-    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    # :s3_credentials => "#{Rails.root}/config/s3.yml",
     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
     :url => "/system/:attachment/:id/:style/:filename",
     :default_url => '/assets/missing_:style.jpg'
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,13 @@ Nate::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
